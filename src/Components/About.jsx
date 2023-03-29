4de1fe80ed599b8d7ef80f0b2bfd76dae38083c6
NPotokin <npotokin@gmail.com>
2023-03-29 07:23:14 -0600

    did project section design

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,7 +8,7 @@ function About() {
   const{darkMode, setDarkMode} = useStore();
   
   return (
-    <div className={`w-full h-screen ${darkMode ? 'bg-cyan-900' : 'bg-sky-100'}`}>
+    <div className={`w-full h-full ${darkMode ? 'bg-cyan-900' : 'bg-sky-100'}`}>
     <div 
     id='about'
     className={`max-w-[1040px] m-auto md:pl-20 p-4 py-16 ${darkMode ? 'bg-cyan-900' : 'bg-sky-100'}`}>
@@ -26,15 +26,15 @@ function About() {
                 <p className='flex flex-wrap gap-4 flex-row justify-start text-xs md:text-sm'>
                   <span className={`inline-block px-2 py-1 font-semibold rounded-lg 
                   ${darkMode 
-                        ? ' text-cyan-900 bg-sky-300' 
-                        : ' text-sky-100 bg-cyan-800'}`}>{item.year}</span>
+                    ? ' text-cyan-900 bg-sky-300' 
+                    : ' text-sky-100 bg-cyan-800'}`}>{item.year}</span>
                   <span className={`text-lg font-semibold 
                   ${darkMode 
                     ? 'text-slate-100' 
                     : 'text-slate-800'}`}>{item.title}</span>
                   <span className={`my-1 text-sm font-normal leading-none ${darkMode 
-          ? 'text-sky-300' 
-          : 'text-cyan-800'}`}>{item.duration}</span>
+                    ? 'text-sky-300' 
+                    : 'text-cyan-800'}`}>{item.duration}</span>
                 </p>
                 <p className={`my-2 text-base font-normal ${darkMode 
                     ? 'text-slate-100' 
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,36 +1,52 @@
 import React from 'react'
+import useStore from '../Utilities/Store'
 
 
 function Projects() {
-  return (
-    <div 
-    id='projects'
-    className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-      <h1 className='text-4xl font-bold text-center text-slate-800'>Projects</h1>
-      <p className='py-8 text-center'>
-        Project description or bla bla bla
-      </p>
-      <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-12'>
-        <div className='relative flex items-center justify-center h-auto w-full shadow-xl
-          shadow-cyan-800 rounded-xl group hover:bg-cyan-200'>
-          <img 
-          className='rounded-xl group-hover:opacity-30 max-w-[256px] max-h-[256px] object-fill'
-          src='../src/Assets/weatherapp.png' 
-          alt="" />
-          <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-            <h3 className='text-2xl font-bold text-sky-100 tracking-wider text-center'>
-              Title
-            </h3>
-            <p>Short description</p>
-            <div className='container flex flex-row justify-around'>
-              <button className='p-1 m-1 rounded-lg bg-cyan-800'>Example</button>
-              <button className='p-1 m-1 rounded-lg bg-cyan-800'>Code</button>
-            </div>
 
+  const{darkMode, setDarkMode} = useStore();
+
+  return (
+    <div className={`w-full  ${darkMode ? 'bg-cyan-900' : 'bg-sky-100'}`}>
+      <div 
+      id='projects'
+      className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <h1 className={`text-4xl md:text-5xl lg:text-6xl pb-6 font-bold text-center ${darkMode 
+            ? 'text-sky-300' 
+            : 'text-cyan-800'}`}>
+            Projects
+        </h1>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-12'>
+          <div className={`relative flex items-center justify-center h-full w-full shadow-lg
+             rounded-xl group ${darkMode ? ' shadow-sky-300 hover:bg-cyan-900' : ' shadow-cyan-800 hover:bg-sky-100'}`}>
+            <img 
+            className='rounded-xl group-hover:opacity-30 max-w-[256px] max-h-[256px] object-fill'
+            src='../src/Assets/weatherapp.png' 
+            alt="" />
+            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+              <h3 className={`text-2xl font-bold  tracking-wider text-center 
+              ${darkMode ? 'text-sky-300' : 'text-cyan-800'}`}>
+                Title
+              </h3>
+              <p className={`text-xl font-medium w-[200px] p-1  text-justify 
+              ${darkMode ? 'text-slate-100' : 'text-slate-800'} `}>Fetching API to return local time, weather, and 2 week forecast</p>
+              <div className='container flex flex-row justify-around p1 m-1'>
+                <button 
+                className={`p-2 m-1 rounded-xl font-medium text-lg shadow-md hover:scale-110  hover:shadow-2xl duration-700 border
+                 ${darkMode 
+                 ? 'text-sky-300 shadow-sky-300 bg-cyan-900 border-sky-300' 
+                 : 'text-cyan-800 shadow-cyan-800 bg-sky-100 border-cyan-800'}`}>Example</button>
+                <button className={`p-2 m-1 rounded-xl font-medium text-lg shadow-md hover:scale-110  hover:shadow-2xl duration-700 border
+                 ${darkMode 
+                 ? 'text-sky-300 shadow-sky-300 bg-cyan-900 border-sky-300' 
+                 : 'text-cyan-800 shadow-cyan-800 bg-sky-100 border-cyan-800'}`}>Code</button>
+              </div>
+
+            </div>
           </div>
+          
         </div>
-        
-      </div>
+        </div>
       </div>
   )
 }
